8b8da134c57307a972f5128b0c3f4a0855029612
chi-fu <chi-fu@mail.chungyo.net>
2019-10-15 15:12:21 +0800

    新增搶票系統
    搶票系統現在已經可以使用了

---
+++ orderTicket/orderTicket.buy.php
@@ -0,0 +1,40 @@
+<?php
+include 'functions.php';
+
+//呼叫Redis
+$redis = new Redis();
+$redis->connect("localhost", 6379);
+
+echo '正在為您加入列隊中，請稍後。';
+
+if (isset($_GET['t_id'])) {
+    $ticketid = $_GET['t_id'];
+
+    //如果session中已經有撈過的票券資料直接用，沒有則new
+    $ticketArrname = "ticket".$ticketid;
+    $ticket = (isset($_SESSION[$ticketArrname]) ? ($_SESSION[$ticketArrname]) : (new orderTicket($ticketid)));
+    $_SESSION[$ticketArrname] = $ticket;
+
+    //取得票券總數量
+    $ticketPics = $ticket->getPics();
+
+    //取得票券名稱
+    $ticketName = $ticket->getName();
+
+    //取得user id
+    $u_id = $_SESSION['u_id'];
+
+    //如果列隊還沒滿則加入列隊，並推向下一步等待填寫單子
+    if ($redis->lLen($ticketid) < $ticketPics) {
+        $redis->rPush($ticketName, $u_id);
+        echo '<script>alert("搶票成功");</script>';
+        echo '<script>document.location.href="orderTicket.queue.php?t_id=' . $ticketid . '";</script>';
+    } else {
+        echo '<script>alert("搶票結束");</script>';
+        echo '<script>document.location.href="orderTicket.list.php";</script>';
+    }
+
+    //關閉連線
+    $redis->close();
+
+}
---
+++ orderTicket/orderTicket.php
@@ -0,0 +1,54 @@
+<?php
+include __DIR__ . '/config/db_config.php';
+
+class orderTicket
+{
+    private $t_id;
+    private $t_name;
+    private $t_price;
+    private $t_content;
+    private $t_pics;
+
+    function __construct($t_id)
+    {
+        global $conn;
+        $sql = "SELECT * FROM `ticket` WHERE `t_id` =" . $t_id;
+        $result = mysqli_query($conn, $sql);
+        if ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+            $this->t_id = $row['t_id'];
+            $this->t_name = $row['t_name'];
+            $this->t_price = $row['t_price'];
+            $this->t_content = $row['t_content'];
+            $this->t_pics = $row['t_pics'];
+        }
+
+        $redis = new Redis();
+        $redis->connect("localhost", 6379);
+        $redis->set("lastTicket", $this->t_pics);
+    }
+
+    function getID()
+    {
+        return $this->t_id;
+    }
+
+    function getName()
+    {
+        return $this->t_name;
+    }
+
+    function getPrice()
+    {
+        return $this->t_price;
+    }
+
+    function getContent()
+    {
+        return $this->t_content;
+    }
+
+    function getPics()
+    {
+        return $this->t_pics;
+    }
+}
