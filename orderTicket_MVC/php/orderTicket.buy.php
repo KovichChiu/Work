a16ef354be75851945f3faca0af04af02a53b8da
chi-fu <chi-fu@mail.chungyo.net>
2019-10-21 17:59:37 +0800

    Bug fixed.
    增加一層防止超賣的防護。

--- orderTicket_MVC/php/orderTicket.buy.php
+++ orderTicket_MVC/php/orderTicket.buy.php
@@ -1,23 +1,22 @@
 <?php
 include __DIR__ . '/orderTicket.php';
-session_start();
 
 if (isset($_POST['u_id']) && isset($_POST['u_acc']) && isset($_POST['u_name']) && isset($_POST['t_id'])) {
-
-    //call redis
-    $redis = new Redis();
-    $redis->connect("localhost", 6379);
-
     //接資料
     $u_id = $_POST['u_id'];
     $u_acc = $_POST['u_acc'];
     $u_name = $_POST['u_name'];
     $t_id = $_POST['t_id'];
 
+    //call redis
+    $redis = new Redis();
+    $redis->connect("localhost", 6379);
+
     //製作ticket
     $ticket = new orderTicket($t_id);
 
-    $r_Listkey = $ticket->getName();    //redis list名
+    $r_Listkey = $ticket->getName();    //redis list名(列隊1)
+    $r_Listnum = "Listnum" . $t_id;       //redis list名(列隊2)
     $r_Remkey = "ticket" . $t_id;       //redis remaining名
 
     //設定票數(如果已經存在則不設)
@@ -26,11 +25,20 @@ if (isset($_POST['u_id']) && isset($_POST['u_acc']) && isset($_POST['u_name']) &
         $redis->set($r_Remkey, $t_allPics);
     }
 
-    //加入列隊(若"列隊長度"大於等於"剩餘票數"則不列隊)
-    if ($redis->lLen($r_Listkey) < $redis->get($r_Remkey)) {
+    //加入列隊(若"列隊長度"大於等於"總票數"則不列隊)
+    if ($redis->lLen($r_Listnum) < $t_allPics) {
 
-        //加入列隊
+        //加入列隊1、列隊2(1是排隊、2是號碼牌)
         $redis->rPush($r_Listkey, $u_id);
+        $redis->rPush($r_Listnum, $u_id);
+
+        //若加入後超過長度則拔除
+        if ($redis->lLen($r_Listnum) > $t_allPics) {
+            $redis->rPop($r_Listnum);
+            echo "noTicket";
+        } else {
+            echo $redis->lLen($r_Listnum);
+        }
 
         while (true) {
             //從列隊第一個撈值比對(取前10位)
